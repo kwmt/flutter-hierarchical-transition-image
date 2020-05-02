3a7b887fab77f26c6c1cbd3dc0a0f165c2e53fbb
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-05-02 13:13:59 +0900

    first commit

---
+++ example/android/app/src/main/kotlin/net/kwmt27/hierarchical_transition_image_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.kwmt27.hierarchical_transition_image_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/hierarchical_transition_image.dart
@@ -0,0 +1,7 @@
+library hierarchical_transition_image;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
