551aa96ffacab8ee48547db56ffe2f1d9fd2857a
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-05-09 13:11:39 +0900

    added test

--- test/hierarchical_transition_image_test.dart
+++ test/hierarchical_transition_image_test.dart
@@ -1,13 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:hierarchical_transition_image/hierarchical_transition_image.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  testWidgets(
+      'The widget HierarchicalTransitionImageStatefulWidget is Exteneded has a close icon',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+        buildTestableWidget(TestWidget("tag", "assets/gopher.png")));
+    expect(find.byIcon(Icons.close), findsOneWidget);
   });
+}
+
+class TestWidget extends HierarchicalTransitionImageStatefulWidget {
+  TestWidget(String tag, String image) : super(tag, image);
+
+  @override
+  State<StatefulWidget> createState() => _TestState();
+}
+
+class _TestState extends HierarchicalTransitionDestinationState {
+  @override
+  Widget build(BuildContext context) {
+    return destinationContainer(
+        Container(child: Image.asset(this.widget.image)));
+  }
+}
+
+// https://stackoverflow.com/a/55696172
+Widget buildTestableWidget(Widget widget) {
+  return MediaQuery(data: MediaQueryData(), child: MaterialApp(home: widget));
 }
